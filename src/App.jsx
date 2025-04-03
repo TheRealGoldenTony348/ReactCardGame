8e5e0a276d14bc21e0ad808c00e457e1690edb32
TheRealGoldenTony348 <antoniwolski348@gmail.com>
2025-04-03 09:16:07 +0200

    fixed fetching

--- src/App.jsx
+++ src/App.jsx
@@ -9,46 +9,41 @@ export default function App(){
         "shuffled": Boolean,
         "remaining": Number
     });
-    const [cardImage, setCardImage] = useState({
-        "code": "0N",
-        "image": "https://placehold.co/600x400",
+    const [cardValue, setCardValue] = useState({
+        "code": String,
+        "image": String,
         "images": {
-            "svg": "https://placehold.co/600x400",
-            "png": "https://placehold.co/600x400"
+            "svg": String,
+            "png": String
         },
-        "value": "0",
-        "suit": "none"
+        "value": String,
+        "suit": String
     });
-    const [drawnCard, setDrawnCard] = useState({
+    const [drawnCards, setDrawnCards] = useState({
         "success": Boolean,
         "deck_id": String,
         "cards": [
-            cardImage
+            cardValue
         ],
         "remaining": Number
     });
     const [isDeckShuffled, setIsDeckShuffled] = useState(false);
     const [isCardDone, SetIsCardDone] = useState(false);
-    const [card, setCard] = useState(null);
     
 
-    const fetchDeckOnClick = () => {
-        fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+    const fetchDeckOnClick = async () => {
+        await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
             .then((response) => response.json())
             .then((result) => setDeckData(result))
             .catch((error) => console.error("Error fetching data: ", error));
         setIsDeckShuffled(true);
     }
 
-    const fetchCardFromDeck = () => {
-        fetch(`https://deckofcardsapi.com/api/deck/${deckData.deck_id}/draw/?count=1`)
+    const fetchCardFromDeck = async () => {
+        await fetch(`https://deckofcardsapi.com/api/deck/${deckData.deck_id}/draw/?count=4`)
             .then((response) => response.json())
-            .then((result) => setDrawnCard(result))
+            .then((result) => setDrawnCards(result))
             .catch((error) => console.error("Error fetching data: ", error));
-        setCardWhenDoneFetching();
-    }
-    const setCardWhenDoneFetching = () => {
-        setCard(<Card image={cardImage.image} value={cardImage.value} suit={cardImage.suit} />);
         SetIsCardDone(true);
     }
 
@@ -58,9 +53,11 @@ export default function App(){
         <button onClick={fetchDeckOnClick}>Shuffle a Deck</button>
         {deckData ? <pre>{JSON.stringify(deckData, null, 2)}</pre> : <p>Shuffle a deck</p>}
         {isDeckShuffled ? <button onClick={fetchCardFromDeck}>Draw a Card from the Deck</button> : <div></div>}
-        {isCardDone ? card : <p>Draw a card</p>}
+        {isCardDone ? drawnCards.cards.map((card, i) => (
+            <Card key={i} image={card.image} value={card.value} suit={card.suit}></Card>
+        )) : <p>Draw a card</p>}
 
-        <button onClick={console.log(cardImage, drawnCard, deckData)}>CHECK</button>
+        <button onClick={console.log(cardValue, drawnCards, deckData)}>CHECK</button>
         </>
     );
 }
